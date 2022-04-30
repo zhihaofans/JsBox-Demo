b0171b35f28caeff233e32a8b2f29da0d1ac0fd1
zhihaofans <zhihaofans@hotmail.com>
2022-04-30 11:14:55 +0800

    更新

--- scripts/ui/list.js
+++ scripts/ui/list.js
@@ -16,12 +16,42 @@ class StringList {
           },
           layout: $layout.fill,
           events: {
-            didSelect: (_sender, indexPath, _data) => {
+            didSelect: (sender, indexPath, _data) => {
               const section = indexPath.section;
               const row = indexPath.row;
               $console.info({
                 section,
                 row
+              });
+            }
+          }
+        }
+      ]
+    });
+  }
+  loadingOnItemClick() {
+    $ui.push({
+      props: {
+        title: title
+      },
+      views: [
+        {
+          type: "list",
+          props: {
+            id,
+            data
+          },
+          layout: $layout.fill,
+          events: {
+            didSelect: (sender, indexPath, _data) => {
+              const section = indexPath.section;
+              const row = indexPath.row;
+              $console.info({
+                section,
+                row
+              });
+              sender.cell(indexPath).startLoading({
+                color: $color("#FF0000")
               });
             }
           }
